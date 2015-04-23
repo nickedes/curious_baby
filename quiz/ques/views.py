2300b29256d570572e9fbbf4d70fe8ea12722188
nikhil <nkmittal4994@gmail.com>
2015-04-23 21:54:46 +0530

    trying models only for foreign key

--- quiz/ques/views.py
+++ quiz/ques/views.py
@@ -1,11 +1,11 @@
 from django.http import Http404
-from django.shortcuts import render, render_to_response, RequestContext
+from django.shortcuts import render
 from django.http import HttpResponse
 # from django.template import RequestContext, loader
 from .models import questions
-from django.db import connection
-from .models import options
-from django import forms
+# from django.db import connection
+# from .models import options
+# from django import forms
 # from .forms import OptionsForm
 
 
@@ -19,16 +19,9 @@ def detail(request, question_id):
     # form = OptionsForm(request.POST or None)
     try:
         q = questions.objects.get(pk=question_id)
-        print(question_id)
-        cursor = connection.cursor()
-        cursor.execute(
-            "select choice_text from ques_choice where qid_id = %s",[question_id])
-        CHOICES = cursor.fetchall()
-        print(CHOICES)
-        ans = forms.ChoiceField(choices=CHOICES)
     except questions.DoesNotExist:
         raise Http404("Question does not exist")
-    return render(request, 'ques/detail.html', {'question': q, 'form': ans})
+    return render(request, 'ques/detail.html', {'question': q})
 
 
 def results(request, question_id):
