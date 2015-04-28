207a53702004e82693c86f31d408e7ce3835aa99
nikhil <nkmittal4994@gmail.com>
2015-04-28 19:28:59 +0530

    choice selection and entry in answer table

--- quiz/ques/views.py
+++ quiz/ques/views.py
@@ -1,9 +1,9 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 # from django.template import RequestContext, loader
-from .models import questions
-# from django.db import connection
+from .models import questions, choice
+from django.db import connection
 # from .models import options
 # from django import forms
 # from .forms import OptionsForm
@@ -21,7 +21,19 @@ def detail(request, question_id):
         q = questions.objects.get(pk=question_id)
     except questions.DoesNotExist:
         raise Http404("Question does not exist")
-    return render(request, 'ques/detail.html', {'question': q})
+    if request.GET.get('choice') is None:
+        return render(request, 'ques/detail.html', {'question': q})
+    else:
+        pop = request.GET.get('choice')
+        print(question_id)
+        cursor = connection.cursor()
+        cursor.execute("INSERT into ques_answer (qid_id, ansid) VALUES (%d,%d)" % (int(question_id) , int(pop)))
+        # cursor.execute("INSERT into ques_answer (ansid,qid_id) VALUES (%d)" %int(question_id))
+
+        return render_to_response('ques/ans.html')
+        # cursor.execute('COMMIT')
+        # return render(request, 'ques/detail.html', {'question': q})
+
 
 
 def results(request, question_id):
@@ -31,3 +43,8 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+
+# def response(request, choice_id):
+
+#     return render(request, 'ques/response.html',{'c':choice_id})
