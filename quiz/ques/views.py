bedb669f0d5a4f36d977d635068de4582d9cbd2e
nikhil <nkmittal4994@gmail.com>
2015-05-20 13:26:55 +0530

    Add new Question. Can add new question and it's choice.

--- quiz/ques/views.py
+++ quiz/ques/views.py
@@ -1,8 +1,7 @@
 from django.http import Http404
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
-from .models import questions, choice, answer
-from django.db import connection
+from .models import questions, answer, choice
 
 
 def index(request):
@@ -36,4 +35,14 @@ def vote(request, question_id):
 
 
 def add(request):
-    return render_to_response('ques/add.html')
+    if request.GET.get('question') is None:
+        return render_to_response('ques/add.html')
+    else:
+        if request.GET.get('choice') is not None:
+            # TODO: get the next qid for entry in db.
+            obj = questions(question=request.GET['question'], qid=6) 
+            obj.save()
+            # TODO: get cid for choice entry.
+            choice_obj = choice(cid=15,qid=obj,choice_text=request.GET['choice'],votes=0)
+            choice_obj.save()
+            return render_to_response('ques/add.html')
