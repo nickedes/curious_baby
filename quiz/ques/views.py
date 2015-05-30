676824b069976c6cf2d2d4626c58108eddbbb73f
nikhil <nkmittal4994@gmail.com>
2015-05-30 14:11:41 +0530

    New Feature Trying to modify all the choices of a question.In progress.

--- quiz/ques/views.py
+++ quiz/ques/views.py
@@ -18,6 +18,10 @@ def detail(request, question_id):
         raise Http404("Question does not exist")
     if request.GET.get('choice') is None:
         return render(request, 'ques/detail.html', {'question': q})
+
+    elif request.GET.get('') is not None:
+        return render_to_response('ques/change_choices.html')
+    
     else:
         pop = request.GET['choice']
         obj = answer(qid=q, ansid=int(pop))
@@ -47,7 +51,7 @@ def add(request):
             # TODO: get cid for choice entry.
             latest_choice = choice.objects.order_by('cid').last()
             choice_obj = choice(
-                cid=latest_choice.cid + 1, qid=obj, 
+                cid=latest_choice.cid + 1, qid=obj,
                 choice_text=request.GET['choice'], votes=0)
             choice_obj.save()
             return render_to_response('ques/add.html')
